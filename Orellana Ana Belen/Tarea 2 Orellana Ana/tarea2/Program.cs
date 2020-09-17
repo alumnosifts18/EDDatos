a6cee3072cf33578f9aca2120ef96bcae86e27c9
Ana18 <anaborellana1184@gmail.com>
2020-09-17 18:26:48 -0300

    ejercicios 2 y 3

---
+++ Orellana Ana Belen/Tarea 2 Orellana Ana/tarea2/Program.cs
@@ -0,0 +1,329 @@
+﻿/*
+
+//Tarea 2: Crea un programa que diga el resultado de sumar 200 y 56.
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(200 + 56);
+        }
+    }
+}
+
+
+//Tarea 3: Haz un programa que calcule el producto de los números 13 y 12.
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(13 * 12);
+        }
+    }
+}
+
+
+//Tarea 4: Un programa que calcule la diferencia (resta) entre 321 y 213
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(312 - 213);
+        }
+    }
+}
+
+
+//Tarea 5: Calcular el resultado de (20+5) % 6
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((20 + 5) % 6);
+        }
+    }
+}
+
+
+//Tarea 6: Crea un programa que calcule el producto de los números 12 y 102, usando variables. 
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 12;
+            int num2 = 102;
+            Console.WriteLine(num1 * num2);
+        }
+    }
+}
+
+
+//Tarea 7: Crea un programa que calcule la suma de 200 y 1111, usando variables.
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 200;
+            int num2 = 1111;
+            Console.WriteLine(num1 + num2);
+        }
+    }
+}
+
+
+//Tarea 8: Crea un programa que calcule el producto de los números 10 y 25, usando variables llamadas "numero1" y "numero2".
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1 = 10;
+            int numero2 = 25;
+            Console.WriteLine(numero1 + numero2);
+        }
+    }
+}
+
+
+//Tarea 9: Crea un programa que calcule el producto de dos números introducidos por el usuario.
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese numero 1: ");
+            String intro1 = Console.ReadLine();
+            int num1 = Convert.ToInt16(intro1);
+
+            Console.WriteLine("Ingrese numero 2: ");
+            String intro2 = Console.ReadLine();
+            int num2 = Convert.ToInt16(intro2);
+
+            Console.WriteLine(num1 * num2);
+        }
+    }
+}
+
+
+//Tarea 10: Crea un programa que pida al usuario un número entero y diga si es par (pista: habrá que comprobar si el resto que se obtiene al dividir entre dos es cero: if (x % 2 == 0) …).
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero: ");
+            String intro1 = Console.ReadLine();
+            int num1 = Convert.ToInt16(intro1);
+
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("El número es par");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+            }
+        }
+    }
+}
+
+
+//Tarea 11: Crea un programa que pida al usuario dos números enteros y diga cuál es el mayor de ellos.
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un entero: ");
+            String intro1 = Console.ReadLine();
+            int num1 = Convert.ToInt16(intro1);
+
+            Console.WriteLine("Ingrese otro entero: ");
+            String intro2 = Console.ReadLine();
+            int num2 = Convert.ToInt16(intro2);
+
+            if (num1 > num2)
+            {
+                Console.WriteLine("El numero 1 es Mayor");
+            }
+            else
+            {
+                Console.WriteLine("El número 2 es Mayor");
+            }
+        }
+    }
+}
+
+
+//Tarea 12: Crea un programa que pida al usuario dos números enteros. Si el segundo no es cero, mostrará el resultado de dividir entre el primero y el segundo. Por el contrario, si el segundo número es cero, escribirá "Error: No se puede dividir entre cero".
+
+using System;
+
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero entero: ");
+            String intro1 = Console.ReadLine();
+            int num1 = Convert.ToInt16(intro1);
+
+            Console.WriteLine("Ingrese otro numero entero: ");
+            String intro2 = Console.ReadLine();
+            int num2 = Convert.ToInt16(intro2);
+
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero");
+            }
+            else
+            {
+                Console.WriteLine(num1 / num2);
+            }
+        }
+    }
+}
+
+
+//Tarea 13: Crea un programa que pida un número del 1 al 5 al usuario, y escriba el nombre de ese número, usando "switch" (por ejemplo, si introduce "1", el programa escribirá "uno").
+
+using System;
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Ingrese un numero del 1 al 5: ");
+            String intro = Console.ReadLine();
+            int num = Convert.ToInt16(intro);
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Uno");
+                    break;
+                case 2:
+                    Console.WriteLine("Dos");
+                    break;
+                case 3:
+                    Console.WriteLine("Tres");
+                    break;
+                case 4:
+                    Console.WriteLine("Cuatro");
+                    break;
+                case 5:
+                    Console.WriteLine("Cinco");
+                    break;
+            }
+        }
+    }
+
+}
+
+
+//Tarea 14: Crea un programa que escriba en pantalla los números del 1 al 10, usando "while".
+
+using System;
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int num = 1;
+
+            while (num <= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+    }
+}
+
+
+//Tarea 15: Crea un programa que escriba en pantalla los números del 1 al 10, usando "do… while".
+
+using System;
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            int num = 1;
+            do
+            {
+                Console.WriteLine(num);
+                num++;
+            } while (num <= 10);
+        }
+    }
+}
+
+
+//Tarea 16: Crea un programa que muestre los números del 10 al 20, ambos incluidos, usando “for”.
+
+using System;
+namespace Ejercicios1
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            for (int i = 10; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+*/
---
+++ Orellana Ana Belen/Tarea 3 Orellana Ana/Tarea 3/Program.cs
@@ -0,0 +1,248 @@
+﻿/*Tarea 1: Crea un programa que pida al usuario su identificador y su contraseña (ambos
+numéricos), y no le permita seguir hasta que introduzca como identificador "1234" y como
+contraseña "1111".
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int identificador = 0;
+            int contrasena = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese ID: ");
+                identificador = Convert.ToInt16(Console.ReadLine());
+                Console.WriteLine("Ingrese contraseña: ");
+                contrasena = Convert.ToInt16(Console.ReadLine());
+            } while (identificador != 1234 && contrasena != 1111);
+        }
+    }
+}
+
+
+Tarea 2: Crea un programa calcule cuantas cifras tiene un número entero positivo (pista: se
+puede hacer dividiendo varias veces entre 10).
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String numero = "";
+
+            Console.WriteLine("Ingrese número: ");
+            numero = Console.ReadLine();
+
+            Console.WriteLine("El número tiene: " + numero.Length + " cifras.");
+        }
+    }
+}
+
+Tarea 3: Crea un programa que muestre la tabla de multiplicar del 9.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(9 + "x" + i + " = " + i * 9);
+            }
+        }
+    }
+}
+
+Tarea 4: Crea un programa que contenga un bucle sin fin que muestre los números enteros
+positivos a partir del uno.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1;
+            for ( ; ; )
+            {
+                Console.WriteLine(numero);
+                numero++;
+            }
+        }
+    }
+}
+
+
+Tarea 5: Crea un programa que contenga un bucle sin fin que escriba "Hola " en pantalla, sin
+avanzar de línea.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String hola = "Hola";
+            for ( ; ; )
+            {
+                Console.Write(hola);
+            }
+        }
+    }
+}
+
+Tarea 6: ¿Cuál sería el resultado de las siguientes operaciones? a=5; b=++a; c=a++; b=b*5;
+a=a*2; Crea un programa que lo resuelva.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("a=5: " + a);
+            int b = ++a;
+            Console.WriteLine("b=++a: " + b);
+            int c = a++;
+            Console.WriteLine("c=a++: " + c);
+            Console.WriteLine("b=b*5: " + (b*=5));
+            Console.WriteLine("a = a * 2: " + (a*=2));
+        }
+    }
+}
+
+Tarea 7: ¿Cuál sería el resultado de las siguientes operaciones? a=5; b=a+2; b-=3; c=-3; c*=2;
+++c; a*=b; Crea un programa que te lo muestre.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("a=5: " + a);
+            int b = a + 2;
+            Console.WriteLine("b=a+2: " + b);
+            Console.WriteLine("b-=3: " + (b-=3));
+            int c = -3;
+            Console.WriteLine("c=-3: " + c);
+            Console.WriteLine("c*=2: " + (c*=2));
+            Console.WriteLine("++c: " + ++c);
+            Console.WriteLine("a*=b: " + (a*=b)); 
+
+        }
+    }
+}
+
+Tarea 8: Crea un programa que pida una letra al usuario y diga si se trata de una vocal.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo letra = Console.ReadKey();
+            Console.WriteLine("Ingrese una letra: ");
+
+            if (letra.Key == ConsoleKey.A || letra.Key == ConsoleKey.E || letra.Key == ConsoleKey.I || letra.Key == ConsoleKey.O || letra.Key == ConsoleKey.U)
+            {
+                Console.WriteLine("Es vocal");
+            }
+            else
+            {
+                Console.WriteLine("No es vocal");
+            }
+        }
+    }
+}
+
+Tarea 9: Crear un programa que pida al usuario un nombre y una contraseña.
+La contraseña se debe introducir dos veces. Si las dos contraseñas no son iguales,
+se avisará al usuario y se le volverán a pedir las dos contraseñas.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            String pass1 = "0";
+            String pass2 = "1";
+
+            while (pass1 != pass2)
+            {
+                Console.WriteLine("Ingrese Nombre: ");
+                name = Console.ReadLine();
+
+                Console.WriteLine("Ingrese Password: ");
+                pass1 = Console.ReadLine();
+
+                Console.WriteLine("Reingrese Password: ");
+                pass2 = Console.ReadLine();
+
+                if (pass1 != pass2)
+                {
+                    Console.WriteLine("Se debe ingresar los datos nuevamente");
+                }
+            }
+        }
+    }
+}
+
+Tarea 10: Crear un programa que pida al usuario su nombre, y le diga "Hola" si
+se llama "Martín", o bien le diga "No te conozco" si teclea otro nombre.
+
+using System;
+
+namespace Tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            Console.WriteLine("Ingrese Nombre: ");
+            name = Console.ReadLine();
+
+            if (name != "Martín")
+            {
+                Console.WriteLine("No te conozco");
+            }
+            else
+            {
+                Console.WriteLine("Hola");
+            }
+        }
+    }
+}
+ 
+ */
+
+
