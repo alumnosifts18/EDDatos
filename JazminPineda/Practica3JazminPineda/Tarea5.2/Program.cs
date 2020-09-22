b42f692fa4687080eac8831a0bce07ee1e455082
Jazmin <jppinedac940@hotmail.com>
2020-09-21 21:37:34 -0300

    Finalizo Tarea 5 Funciones

---
+++ JazminPineda/Practica3JazminPineda/Tarea5.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5._2
+{
+    class Program
+    {
+        public static int Menor(int num1, int numer2)
+        {
+            int otro = num1;
+            if (num1 < numer2)
+                otro = num1;
+            else if (numer2 < num1)
+                otro = numer2;
+
+            return otro;
+        }
+        
+                static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe primer numero entero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe segundo numero entero");
+            int numer2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(Menor(num1, numer2));
+
+            Console.Beep(666, 666);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JazminPineda/Practica3JazminPineda/Tarea5.6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea5._6
+{
+    class Program
+    {
+        public static int SumcaCifras(string cifra)
+        {
+            int suma_acum = 0;
+            for (int pocision = 0; pocision < cifra.Length; pocision++)
+            {
+                suma_acum = int.Parse(cifra[pocision].ToString()) + suma_acum;
+            }
+                
+            return suma_acum;
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Escribe una cifra ");
+            string cifra = Console.ReadLine();
+            Console.WriteLine("La sumatoria de la cifra es {0}", SumcaCifras(cifra));
+            Console.ReadLine();
+
+        }
+    }
+}
